a2beefff0ae35d9bce68b022edc6b337694db349
farisalfrz <faris.alfarizi109@gmail.com>
2023-07-24 14:07:26 +0700

    Adding search features for user

---
+++ src/GameStore.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public class GameStore {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        
+//        Dashboard adminframe = new Dashboard();
+//        adminframe.setVisible(true);
+        
+//        UserFrame userframe = new UserFrame();
+//        userframe.setVisible(true);
+    }
+}
